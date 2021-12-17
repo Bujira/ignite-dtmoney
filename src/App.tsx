244b09b7f1cd8682146a4133d39a6e474bee7c4b
Yuri Correa <yurifcorrea@gmail.com>
2021-12-16 21:29:13 -0300

    commit 4: header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Header } from "./components/Header"
 import { GlobalStyles } from "./styles/global"
 
 export function App() {
   return (
-    <div className="App">
+    <>
       <GlobalStyles />
-      <h1>Hello World!</h1>
-    </div>
+      <Header />
+    </>
   );
 }
