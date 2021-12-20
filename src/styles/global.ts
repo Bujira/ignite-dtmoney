fc02850dd488109c445b6a44dace9e178f3052a4
Yuri Correa <yurifcorrea@gmail.com>
2021-12-20 13:42:16 -0300

    commit 11: styling the modal

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -76,4 +76,18 @@ export const GlobalStyles = createGlobalStyle`
     position: relative;
     border-radius: 0.25rem;
   }
+
+  .react-modal-close {
+    position: absolute;
+    top: 1.5rem;
+    right: 1.5rem;
+    border: 0;
+    background: transparent;
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.7);
+    }
+  }
 `
